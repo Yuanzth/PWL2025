decf1f2c00d24a6772c85f225d586b12fb42a8b2
Yuanzth <adityayuhanda3001@gmail.com>
2025-03-01 20:57:41 +0700

    JS2&POS Complete

---
+++ PWL_Week3/Js3/PWL_POS/resources/views/products/beauty_health.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Beauty & Health</title>
+</head>
+<body>
+    <h1>Category: Beauty & Health</h1>
+    <p>Daftar produk kecantikan dan kesehatan tersedia di sini.</p>
+</body>
+</html>
---
+++ PWL_Week3/Js3/PWL_POS/resources/views/products/home_care.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home Care</title>
+</head>
+<body>
+    <h1>Category: Home Care</h1>
+    <p>Daftar produk perawatan rumah tersedia di sini.</p>
+</body>
+</html>
