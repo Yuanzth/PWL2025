0ef1e07290c5692e4441a836f083fad0c54c68ea
Yuanzth <adityayuhanda3001@gmail.com>
2025-03-03 00:26:12 +0700

    Js3 Prak3

---
+++ PWL_Week3/Js3/PWL_POS/database/seeders/PenjualanSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class PenjualanSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $data = [];
+        for ($i = 1; $i <= 10; $i++) {
+            $data[] = [
+                'penjualan_id' => $i,
+                'user_id' => 3, // Kasir
+                'pembeli' => 'Pelanggan ' . $i,
+                'penjualan_kode' => 'PNJ' . str_pad($i, 4, '0', STR_PAD_LEFT),
+                'penjualan_tanggal' => Carbon::now()->subDays(rand(0, 10))->toDateTimeString(),
+            ];
+        }
+
+        DB::table('t_penjualan')->insert($data);
+    }
+}
