9c5140e2457f1a8f7a07ac2335e522b2a44dffac
Yuanzth <adityayuhanda3001@gmail.com>
2025-03-03 12:06:08 +0700

    Prak5 Js3

--- PWL_Week3/Js3/PWL_POS/routes/web.php
+++ PWL_Week3/Js3/PWL_POS/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
@@ -25,3 +26,5 @@ Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 Route::get('/sales', [SalesController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
