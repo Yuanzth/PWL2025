da36bea3618b3ee48f9cd9b32f574c3eefea220f
Yuanzth <adityayuhanda3001@gmail.com>
2025-03-03 10:13:17 +0700

    Js3 Prak4

--- PWL_Week3/Js3/PWL_POS/database/migrations/2025_03_02_140822_create_t_stok_table.php
+++ PWL_Week3/Js3/PWL_POS/database/migrations/2025_03_02_140822_create_t_stok_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->integer('stok_jumlah');
             $table->timestamps();
 
-            // Foreign Key
+            // Foreign Keyr
             $table->foreign('barang_id')->references('barang_id')->on('m_barang');
             $table->foreign('user_id')->references('user_id')->on('m_user');
         });
--- PWL_Week3/Js3/PWL_POS/routes/web.php
+++ PWL_Week3/Js3/PWL_POS/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
 
 // Halaman Home
 Route::get('/', [HomeController::class, 'index']);
@@ -22,3 +23,5 @@ Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 
 // Halaman Penjualan
 Route::get('/sales', [SalesController::class, 'index']);
+
+Route::get('/level', [LevelController::class, 'index']);
