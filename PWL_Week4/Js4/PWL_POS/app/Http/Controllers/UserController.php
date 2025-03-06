2f6c2ef30df8e654192138bb1cf40055d35507c9
Yuanzth <adityayuhanda3001@gmail.com>
2025-03-06 13:33:46 +0700

    JS4_Prak1

---
+++ PWL_Week4/Js4/PWL_POS/app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data);
+
+        // update data user dengan Eloquent Model
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); // update data user
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari table m_user
+        return view('user', ['data' => $user]);
+    }
+
+    // public function profile($id, $name)
+    // {
+    //     return view('user.profile', compact('id', 'name'));
+    // }
+}
