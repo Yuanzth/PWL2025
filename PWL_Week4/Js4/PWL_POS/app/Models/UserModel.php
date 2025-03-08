81439fa977ba1ccd6247a62bbbc3cf754f09846d
Yuanzth <adityayuhanda3001@gmail.com>
2025-03-09 02:59:01 +0700

    Done JS4 Prak2.7

--- PWL_Week4/Js4/PWL_POS/app/Models/UserModel.php
+++ PWL_Week4/Js4/PWL_POS/app/Models/UserModel.php
@@ -4,12 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends Model
-{
-    use HasFactory;
-
-    protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan untuk model ini
-    protected $primaryKey = 'user_id';  // Mendefinisikan nama primary key yang digunakan
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+class UserModel extends Model 
+{ 
+    use HasFactory; 
+    
+    protected $table = 'm_user'; 
+    protected $primaryKey = 'user_id'; 
+    
+    /** 
+     * The attributes that are mass assignable. 
+     * @var array 
+     */
+    protected $fillable = ['level_id', 'username', 'nama', 'password']; 
+    
+    public function level(): BelongsTo 
+    { 
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id'); 
+    } 
 }
