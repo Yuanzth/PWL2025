44fe56cefbbefb02b0d64943cb329ed53a9e0840
Yuanzth <adityayuhanda3001@gmail.com>
2025-03-15 22:34:30 +0700

    commit 1st week5

---
+++ PWL_Week5/Js5/PWL_POS/resources/views/products/baby_kid.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Baby & Kid</title>
+</head>
+<body>
+    <h1>Category: Baby & Kid</h1>
+    <p>Daftar produk untuk bayi dan anak tersedia di sini.</p>
+</body>
+</html>
