286b75f835b68d5c09eb8057fc0793b897f36fd2
Yuanzth <adityayuhanda3001@gmail.com>
2025-03-27 23:59:25 +0700

    Praktikum&Tugas 2 – Implementasi Authoriization

--- PWL_Week7/Js7/PWL_POS/routes/web.php
+++ PWL_Week7/Js7/PWL_POS/routes/web.php
@@ -112,4 +112,14 @@ Route::middleware(['auth'])->group(function () {
         Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // menghapus data Barang ajax
         Route::delete('/{id}', [BarangController::class, 'destroy']);
     });
+    // artinya semua route di dalam group ini harus punya role ADM (Administrator)
+    Route::middleware(['authorize:ADM'])->group(function(){
+        Route::get('/level', [LevelController::class, 'index']);
+        Route::post('/level/list', [LevelController::class, 'list']); // untuk list json datatables
+        Route::get('/level/create', [LevelController::class, 'create']);
+        Route::post('/level', [LevelController::class, 'store']);
+        Route::get('/level/{id}/edit', [LevelController::class, 'edit']); // untuk tampilkan form edit
+        Route::put('/level/{id}', [LevelController::class, 'update']); // untuk proses update data
+        Route::delete('/level/{id}', [LevelController::class, 'destroy']); // untuk proses hapus data
+    });
 });
