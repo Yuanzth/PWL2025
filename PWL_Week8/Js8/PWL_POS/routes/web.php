db070c7c112c08dbdcd557840d8bca2896b6101d
Yuanzth <adityayuhanda3001@gmail.com>
2025-03-30 00:35:51 +0700

    Praktikum-2 Export Data Barang ke Excel

--- PWL_Week8/Js8/PWL_POS/routes/web.php
+++ PWL_Week8/Js8/PWL_POS/routes/web.php
@@ -125,6 +125,7 @@ Route::middleware(['auth'])->group(function () {
             Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data barang
             Route::get('/import', [BarangController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // AJAX import excel
+            Route::get('export_excel', [BarangController::class, 'export_excel']); //export excel
         });
     });
     
