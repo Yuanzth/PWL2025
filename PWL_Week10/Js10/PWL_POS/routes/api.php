dbe33a2e693bd18d850f6efb4aaf271639c66fa3
Yuanzth <adityayuhanda3001@gmail.com>
2025-04-26 02:20:40 +0700

    JS10 : Tugas Tabel m_user

--- PWL_Week10/Js10/PWL_POS/routes/api.php
+++ PWL_Week10/Js10/PWL_POS/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -28,3 +29,10 @@ Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('
  Route::get('levels/{level}', [LevelController::class, 'show']);
  Route::put('levels/{level}', [LevelController::class, 'update']);
  Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+
+ Route::get('users', [UserController::class, 'index']);
+ Route::post('users', [UserController::class, 'store']);
+ Route::get('users/{user}', [UserController::class, 'show']);
+ Route::put('users/{user}', [UserController::class, 'update']);
+ Route::delete('users/{user}', [UserController::class, 'destroy']);
